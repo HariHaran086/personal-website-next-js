c12f8a5561ffe8e299f73885487326dd9cd2375e
Hariharan Manochrasa <harishmanochrasa@gmail.com>
2025-04-14 10:42:29 +0530

    Add Portfolio component and integrate it into the Home page

---
+++ components/portfolio.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <>
+        <div className="flex flex-col items-center justify-center h-auto">
+            <h1 className="text-4xl font-bold">test</h1>
+            <p className="mt-4 text-lg">testp</p>
+        </div>
+    </>
+  )
+}
+
+export default Portfolio
